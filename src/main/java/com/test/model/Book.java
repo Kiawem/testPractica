e18e864b881f4fb9dd9eb5c2b0248bdf9017b944
Kiawem <103750495+Kiawem@users.noreply.github.com>
2025-06-29 16:16:39 +0300

    tests for Books Done

--- src/main/java/com/test/model/Book.java
+++ src/main/java/com/test/model/Book.java
@@ -1,12 +1,11 @@
 package com.test.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
+@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Book {
--- src/test/java/com/test/TestPracticaApplicationTests.java
+++ src/test/java/com/test/TestPracticaApplicationTests.java
@@ -1,24 +1,17 @@
 package com.test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.test.controller.BookController;
 import com.test.dto.BookDto;
 import com.test.model.Author;
 import com.test.model.Book;
 import com.test.service.BookService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -30,40 +23,40 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class TestPracticaApplicationTests {
 
-	@Autowired
-	private MockMvc mock;
+    @Autowired
+    private MockMvc mock;
 
-	@MockitoBean
-	private BookService bookService;
+    @MockitoBean
+    private BookService bookService;
 
-	@Autowired
-	private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-	@Test
-	void testAddBook() throws Exception {
-		BookDto dto = new BookDto("Преступление и наказание", 1866, "Роман", "Фёдор Достоевский");
-		Book savedBook = new Book();
-		savedBook.setId(1L);
-		savedBook.setTitle(dto.getTitle());
-		savedBook.setGenre(dto.getGenre());
-		savedBook.setYear(dto.getYear());
-		savedBook.setAuthor(new Author(2L, dto.getAuthorName(), 1821));
-		System.out.println(objectMapper.writeValueAsString(savedBook));
+    @Test
+    void testAddBook() throws Exception {
+        BookDto dto = new BookDto("Преступление и наказание", 1866, "Роман", "Фёдор Достоевский");
+        Book savedBook = new Book();
+        savedBook.setId(1L);
+        savedBook.setTitle(dto.getTitle());
+        savedBook.setGenre(dto.getGenre());
+        savedBook.setYear(dto.getYear());
+        savedBook.setAuthor(new Author(2L, dto.getAuthorName(), 1821));
+        System.out.println(objectMapper.writeValueAsString(savedBook));
 
 
-		when(bookService.addBook(any(BookDto.class))).thenReturn(savedBook);
+        when(bookService.addBook(any(BookDto.class))).thenReturn(savedBook);
 
-		mock.perform(post("/books")
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(objectMapper.writeValueAsString(dto)))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.id").value(1L))
-				.andExpect(jsonPath("$.title").value("Преступление и наказание"))
-				.andExpect(jsonPath("$.genre").value("Роман"));
-	}
+        mock.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.title").value("Преступление и наказание"))
+                .andExpect(jsonPath("$.genre").value("Роман"));
+    }
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
